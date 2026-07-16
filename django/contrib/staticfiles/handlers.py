ece08b38573678ae97ff003e8ee483bebfa8e593
Federico Vela <fedevela@gmail.com>
2026-07-16 16:08:12 -0500

    phase 10 : Malkhut

--- django/contrib/staticfiles/handlers.py
+++ django/contrib/staticfiles/handlers.py
@@ -55,10 +55,7 @@ class StaticFilesHandlerMixin:
             return response_for_exception(request, e)
 
     async def get_response_async(self, request):
-        try:
-            return await sync_to_async(self.serve)(request)
-        except Http404 as e:
-            return await sync_to_async(response_for_exception)(request, e)
+        return await sync_to_async(self.get_response)(request)
 
 
 class StaticFilesHandler(StaticFilesHandlerMixin, WSGIHandler):
