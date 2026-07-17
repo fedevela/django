bf2d003d9a5abfb8aa1d541df0c82cc4f7ff4b4f
Federico Vela <fedevela@gmail.com>
2026-07-16 22:56:11 -0500

    phase 10 : Malkhut

--- django/db/models/sql/query.py
+++ django/db/models/sql/query.py
@@ -675,7 +675,7 @@ class Query(BaseExpression):
         "target", and the model and list of fields being added for that model.
         """
         field_names, defer = self.deferred_loading
-        if not field_names:
+        if not field_names and defer:
             return
         orig_opts = self.get_meta()
         seen = {}
@@ -2083,11 +2083,7 @@ class Query(BaseExpression):
             self.deferred_loading = existing.union(field_names), True
         else:
             # Remove names from the set of any existing "immediate load" names.
-            new_only = existing.difference(field_names)
-            if new_only:
-                self.deferred_loading = new_only, False
-            else:
-                self.clear_deferred_loading()
+            self.deferred_loading = existing.difference(field_names), False
 
     def add_immediate_loading(self, field_names):
         """
