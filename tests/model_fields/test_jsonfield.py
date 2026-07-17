694ab60ef16fc59636347dacb852fc07809ba544
Federico Vela <fedevela@gmail.com>
2026-07-16 19:57:29 -0500

    phase 10 : Malkhut

--- tests/model_fields/test_jsonfield.py
+++ tests/model_fields/test_jsonfield.py
@@ -582,15 +582,27 @@ class TestQuerying(TestCase):
 
     def test_jsonnull_001_sqlite_oracle_isnull_true_includes_record_when_key_absent(self):
         """JSONNULL-001: An absent key is included by key__isnull=True."""
-        pass
+        obj = self.objs[3]
+        self.assertSequenceEqual(
+            NullableJSONModel.objects.filter(pk=obj.pk, value__j__isnull=True),
+            [obj],
+        )
 
     def test_jsonnull_002_sqlite_oracle_isnull_true_excludes_existing_json_null(self):
         """JSONNULL-002: An existing JSON null is excluded by key__isnull=True."""
-        pass
+        obj = self.objs[4]
+        self.assertSequenceEqual(
+            NullableJSONModel.objects.filter(pk=obj.pk, value__j__isnull=True),
+            [],
+        )
 
     def test_jsonnull_003_sqlite_oracle_isnull_false_includes_existing_json_null(self):
         """JSONNULL-003: An existing JSON null is included by key__isnull=False."""
-        pass
+        obj = self.objs[4]
+        self.assertSequenceEqual(
+            NullableJSONModel.objects.filter(pk=obj.pk, value__j__isnull=False),
+            [obj],
+        )
 
     def test_isnull_key(self):
         # key__isnull=False works the same as has_key='key'.
