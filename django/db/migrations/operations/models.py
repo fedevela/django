02f750d7c148db89620f3c72754b5b281bbf5935
Federico Vela <fedevela@gmail.com>
2026-07-17 00:53:31 -0500

    phase 10 : Malkhut

--- django/db/migrations/operations/models.py
+++ django/db/migrations/operations/models.py
@@ -961,6 +961,9 @@ class RenameIndex(IndexOperation):
             from_model_state = from_state.models[app_label, self.model_name_lower]
             old_index = from_model_state.get_index_by_name(self.old_name)
 
+        if old_index.name == self.new_name:
+            return
+
         to_model_state = to_state.models[app_label, self.model_name_lower]
         new_index = to_model_state.get_index_by_name(self.new_name)
         schema_editor.rename_index(model, old_index, new_index)
